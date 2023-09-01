c4d4621d0041392ec2e8ca9f9b0a2561d8539d4c
ugniusmasionis <admin@local.com>
2023-09-02 02:44:26 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+<<<<<<< HEAD
 use std::io;
 use regex::Regex;
 use core::panic;
+=======
+use core::panic;
+use regex::Regex;
+use std::io;
+>>>>>>> 0859bb9 (Initial commit)
 
 fn main() {
     println!("Welcome to the calculator!");
     loop {
         println!("Enter an expression to calculate (e.g. 2 + 2):");
         let mut input = String::new();
+<<<<<<< HEAD
         io::stdin().read_line(&mut input).expect("Failed to read input");
+=======
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+>>>>>>> 0859bb9 (Initial commit)
         let input = input.trim();
 
         if input == "exit" {
@@ -31,4 +43,8 @@ fn calculate(input: &str) -> f64 {
         "/" => num1 / num2,
         _ => panic!("Invalid operator"),
     }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 0859bb9 (Initial commit)
